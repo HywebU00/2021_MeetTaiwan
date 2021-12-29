cff54c26a8bf9e9cdb72a861d168c8510b0dc841
Kai Fu Hsieh <eason@tpe.hyweb.com.tw>
2021-12-29 23:52:20 +0800

    每一頁add userbar

--- js/mt-init.js
+++ js/mt-init.js
@@ -266,7 +266,7 @@ $(window).on('load', function () {
 
     //AOS 通用設定
     AOS.init({
-        duration: 600, // values from 0 to 3000, with step 50ms
+        duration: 500, // values from 0 to 3000, with step 50ms
         offset: 200, // offset (in px) from the original trigger point
         easing: 'ease-out', // default easing for AOS animations
         once: true, // whether animation should happen only once - while scrolling down
