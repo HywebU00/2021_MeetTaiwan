65f6d0a8b31902d13dbed68401de66abfaa36a46
Kai Fu Hsieh <eason@tpe.hyweb.com.tw>
2022-01-03 16:15:45 +0800

    會展獎報名表頁2 3

--- js/mt-init.js
+++ js/mt-init.js
@@ -460,4 +460,35 @@ $(window).on('load', function () {
                     e.preventDefault();
                 });
         });
+    /*------------------------------------*/
+    /////form表單 單個檔案上傳+多個檔案上傳/////
+    /*------------------------------------*/
+    $(document).on('change', '.check_file', function () {
+        var names = [];
+        var length = $(this).get(0).files.length;
+        for (var i = 0; i < $(this).get(0).files.length; ++i) {
+            names.push($(this).get(0).files[i].name);
+        }
+        // $('input[name=file]').val(names);
+        if (length > 2) {
+            var fileName = names.join(', ');
+            $(this)
+                .closest('.upload_grp')
+                .find('.upload_file')
+                .attr('value', length + ' files selected');
+        } else {
+            $(this).closest('.upload_grp').find('.upload_file').attr('value', names);
+        }
+    });
+    /*-----------------------------*/
+    /////     checkbox blur     /////
+    /*-----------------------------*/
+    $('.check_grp').each(function () {
+        $(this)
+            .find('label')
+            .off()
+            .click(function (e) {
+                $(this).find('input[type="checkbox"]').blur();
+            });
+    });
 });
